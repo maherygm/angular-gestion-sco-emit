158e5cf38af0aa2d979eb87b51e009cbed12af77
MAHERY <MAHERY@gmail.com>
2024-01-28 22:28:33 +0300

    projet squellete finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,52 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { Component, NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MainComponent } from './main/main.component';
+import { DashboardComponent } from './main/pages/dashboard/dashboard.component';
+import { HomeComponent } from './main/pages/home/home.component';
+import { NavigationBarComponent } from './main/navigationBar/navigationBar.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './main/pages/login/login.component';
+import { SigninComponent } from './main/pages/signin/signin.component';
+import { NavigationDashboardComponent } from './main/pages/dashboard/layout/navigation-dashboard/navigation-dashboard.component';
+import { ChartPageComponent } from './main/pages/dashboard/content/chart-page/chart-page.component';
+import { ManagementPagesComponent } from './main/pages/dashboard/content/management-pages/management-pages.component';
+import { SettingsPagesComponent } from './main/pages/dashboard/content/settings-pages/settings-pages.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HomeComponent,
+    DashboardComponent,
+    NavigationBarComponent,
+    LoginComponent,
+    SigninComponent,
+    NavigationDashboardComponent,
+    ChartPageComponent,
+    ManagementPagesComponent,
+    SettingsPagesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'dashboard', component: ChartPageComponent },
+      { path: 'dashboard/chart', component: ChartPageComponent },
+      { path: 'dashboard/management', component: ManagementPagesComponent },
+      { path: 'dashboard/settings', component: SettingsPagesComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'signin', component: SigninComponent },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    ]),
   ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/main/pages/dashboard/content/management-pages/management-pages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-management-pages',
+  templateUrl: './management-pages.component.html',
+  styleUrl: './management-pages.component.scss'
+})
+export class ManagementPagesComponent {
+
+}
